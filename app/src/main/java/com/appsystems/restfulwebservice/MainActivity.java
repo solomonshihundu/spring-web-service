1f7a33d2560e8b5d0ebbc6047ee754b12a1a5ea1
Solo <shihundusolomon@gmail.com>
2020-06-23 22:18:38 +0300

    RESTTask

--- app/src/main/java/com/appsystems/restfulwebservice/MainActivity.java
+++ app/src/main/java/com/appsystems/restfulwebservice/MainActivity.java
@@ -2,10 +2,19 @@ package com.appsystems.restfulwebservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -36,6 +45,41 @@ public class MainActivity extends AppCompatActivity
     public void sendRequest(View view)
     {
         final String url ="http://localhost:8080/api/v1/house";
-        
+        new RESTTask().execute(url);
+    }
+
+    class RESTTask extends AsyncTask<String,Void, ResponseEntity<House>>
+    {
+
+        @Override
+        protected ResponseEntity<House> doInBackground(String... uri) {
+
+            final String url = uri[0];
+            RestTemplate restTemplate = new RestTemplate();
+            try{
+
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+                HttpHeaders header = new HttpHeaders();
+
+                String auth = "marco"+":"+"polo";
+                String encodeAuth = Base64.encodeToString(auth.getBytes(),Base64.DEFAULT);
+                String authHeader = "Basic"+ encodeAuth;
+                header.set("Authorization",authHeader);
+
+                HttpEntity<String>entity = new HttpEntity<String>(header);
+
+                ResponseEntity<House>response = restTemplate.exchange(url, HttpMethod.GET,entity,House.class);
+
+                return response;
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
     }
 }
